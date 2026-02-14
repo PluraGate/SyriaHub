ee0108338b2c10a0967696b9609bd2269b8c0cfc
lAvArt <a.v.arch@hotmail.com>
2026-02-14 14:13:01 +0300

    chore: fix missed Navbar prop, update sw.js caching, gitignore dump file

--- components/AboutLayout.tsx
+++ components/AboutLayout.tsx
@@ -50,7 +50,7 @@ export function AboutLayout({ children, user }: AboutLayoutProps) {
 
     return (
         <div className="min-h-screen flex flex-col bg-background dark:bg-dark-bg">
-            <Navbar user={user} />
+            <Navbar />
 
             <main className="flex-1">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
